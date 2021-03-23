3c5ab0e33a49b7156594809d5a5cd90e73f96cda
Souf <soufboulealf@gmail.com>
2021-03-23 10:54:41 +0100

    renames authenticate to authorization dependency

--- cloudhub/provider.go
+++ cloudhub/provider.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	auth "github.com/mulesoft-consulting/cloudhub-client-go/authenticate"
+	auth "github.com/mulesoft-consulting/cloudhub-client-go/authorization"
 )
 
 type ProviderConfOutput struct {
