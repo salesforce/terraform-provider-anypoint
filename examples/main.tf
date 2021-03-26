d357689316d4071ac89279c9d4227ecc38add8fa
Souf <soufboulealf@gmail.com>
2021-03-26 16:59:12 +0100

    updates template

--- examples/main.tf
+++ examples/main.tf
@@ -33,24 +33,77 @@ provider "cloudhub" {
 }
 
 
-resource "cloudhub_vpc" "avpc" {
-  name = "myAwesomeVPC"
-  region = "us-east-1"
-  owner_id = ""
-  cidr_block = "10.0.0.0/20"
-  internal_dns_servers = []
-  internal_dns_special_domains = []
-  is_default = false
-  associated_environments = []
-  shared_with = []
-  //firewall_rules = []
-  //vpc_routes = []
+locals {
+  csv_data = file("${path.module}/csv/vpcs.csv")
+  list_separator = ";"
+
+  vpc_instances = csvdecode(local.csv_data)
 }
+
+resource "cloudhub_vpc" "example" {
+  count = length(local.vpc_instances)
+
+  name = element(local.vpc_instances, count.index).name
+  region = element(local.vpc_instances, count.index).region
+  owner_id = element(local.vpc_instances, count.index).owner_id
+  cidr_block = element(local.vpc_instances, count.index).cidr_block
+  internal_dns_servers = compact(split(local.list_separator, element(local.vpc_instances, count.index).internal_dns_servers))  
+  internal_dns_special_domains = compact(split(local.list_separator, element(local.vpc_instances, count.index).internal_dns_special_domains)) 
+  is_default = element(local.vpc_instances, count.index).is_default
+  associated_environments = compact(split(local.list_separator, element(local.vpc_instances, count.index).associated_environments))
+  shared_with = compact(split(local.list_separator, element(local.vpc_instances, count.index).shared_with))
+
+}
+
+
+output "name" {
+  value = cloudhub_vpc.example[*].name
+}
+
+output "region" {
+  value = cloudhub_vpc.example[*].region
+}
+
+output "cidr_block" {
+  value = cloudhub_vpc.example[*].cidr_block
+}
+
+
+
+
+
 
 #data "cloudhub_vpcs" "all" {}
 
+# resource "cloudhub_vpc" "avpc" {
+#   name = "myAwesomeVPC"
+#   region = "us-east-2"
+#   owner_id = ""
+#   cidr_block = "192.168.0.0/24"
+#   internal_dns_servers = []
+#   internal_dns_special_domains = []
+#   is_default = true
+#   associated_environments = []
+#   shared_with = []
+#   //firewall_rules = []
+#   //vpc_routes = []
+# }
 
-output "created_vpcs" {
-  value = cloudhub_vpc.avpc.id
-  #value = data.cloudhub_vpcs.all
-}
+
+
+
+# output "region" {
+#   value = cloudhub_vpc.avpc.region
+# }
+
+# output "id" {
+#   value = cloudhub_vpc.avpc.id
+# }
+
+# output "name" {
+#   value = cloudhub_vpc.avpc.name
+# }
+
+# output "cidrblock" {
+#   value = cloudhub_vpc.avpc.cidr_block
+# }
