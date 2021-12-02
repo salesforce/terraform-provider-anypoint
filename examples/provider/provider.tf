847f4ecda4a71b7c736defd98ce718e0892245f6
Souf <soufboulealf@gmail.com>
2021-12-02 16:20:59 +0100

    initiates templates and examples, cleans examples folder

---
+++ examples/provider/provider.tf
@@ -0,0 +1,13 @@
+provider "anypoint" {
+  # use either username/pwd or client id/secret to connect to the platform
+
+  username = var.username               # optionally use ANYPOINT_USERNAME env var
+  password = var.password               # optionally use ANYPOINT_USERNAME env var
+
+  client_id = var.client_id             # optionally use ANYPOINT_CLIENT_ID env var
+  client_secret = var.client_secret     # optionally use ANYPOINT_CLIENT_SECRET env var
+
+  # You may need to change the anypoint control plane: use 'eu' or 'us'
+  # by default the control plane is 'us'
+  cplane= var.cplane                    # optionnaly use ANYPOINT_CPLANE env var
+}
---
+++ examples/resources/anypoint_bg/test.auto.tfvars
@@ -0,0 +1,2 @@
+root_org = "aa1f55d6-213d-4f60-845c-207286484cd1"
+owner_id = "18f23771-c78a-4be2-af8f-1bae66f43942"
