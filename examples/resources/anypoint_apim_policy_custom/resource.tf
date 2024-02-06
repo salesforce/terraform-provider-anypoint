b50dd71331604144938fab2463cc752561043157
souf <souf@zoldyck.hxh>
2024-02-06 11:12:53 +0100

    updates documentation for custom policy resource to add more examples

--- examples/resources/anypoint_apim_policy_custom/resource.tf
+++ examples/resources/anypoint_apim_policy_custom/resource.tf
@@ -25,7 +25,7 @@ resource "anypoint_apim_policy_custom" "policy_custom_02" {
   asset_version = "1.4.0"
 
   configuration_data = jsonencode({
-    keySelector= "#[attributes.queryParams['identifier']]",
+    keySelector= "#[attributes.queryParams['identifier']]"
     rateLimits = [
       { maximumRequests = 50
         timePeriodInMilliseconds = 300000
@@ -50,7 +50,7 @@ resource "anypoint_apim_policy_custom" "policy_custom_02" {
 
 
 #Basic Auth Policy Example
-resource "anypoint_apim_policy_custom" "policy_custom_02" {
+resource "anypoint_apim_policy_custom" "policy_custom_03" {
   org_id = var.root_org
   env_id = var.env_id
   apim_id = anypoint_apim_mule4.api.id
@@ -60,8 +60,34 @@ resource "anypoint_apim_policy_custom" "policy_custom_02" {
   asset_version = "1.3.1"
 
   configuration_data = jsonencode({
-    username= "user",
+    username= "user"
     password = "mySupaDupaPasswordWithALotOfCharacters"
   })
+}
 
+#Message Logging Policy Example
+resource "anypoint_apim_policy_custom" "policy_custom_04" {
+  org_id = var.root_org
+  env_id = var.env_id
+  apim_id = anypoint_apim_mule4.api.id
+  disabled = false
+  asset_group_id="68ef9520-24e9-4cf2-b2f5-620025690913"
+  asset_id="message-logging"
+  asset_version = "2.0.1"
+
+  configuration_data = jsonencode({
+    loggingConfiguration = [
+      {
+        itemName = "Default configuration"
+        itemData = {
+          message = "#[attributes.headers['id']]"
+          conditional = "#[attributes.headers['id']==1]"
+          category = "My_Prefix"
+          level = "INFO"
+          firstSection = true
+          secondSection = true
+        }
+      }
+    ]
+  })
 }
