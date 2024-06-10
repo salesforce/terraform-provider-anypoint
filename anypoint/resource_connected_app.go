0d0365a4cefe3daf524a442c453896bc4c6e0f37
souf <soufboulealf@gmail.com>
2024-06-10 12:02:48 +0300

    fixes typo issue on the connected_app resource

--- anypoint/resource_connected_app.go
+++ anypoint/resource_connected_app.go
@@ -268,7 +268,7 @@ func resourceConnectedAppRead(ctx context.Context, d *schema.ResourceData, m int
 	// Is it a "on behalf of user" connected apps?
 	if granttypes := connappinstance["grant_types"]; granttypes != nil && StringInSlice(granttypes.([]string), "client_credentials", true) {
 		// Yes, then load the scopes using connapps/{connapp_id}/scopes
-		if scopes, error := readScopesByConnectedAppId(authctx, orgid, connappid, m); error != nil {
+		if scopes, err := readScopesByConnectedAppId(authctx, orgid, connappid, m); err != nil {
 			diags := append(diags, diag.Diagnostic{
 				Severity: diag.Error,
 				Summary:  "Unable to read connected app " + connappid + " scopes",
